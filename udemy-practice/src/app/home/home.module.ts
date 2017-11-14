38bac8cfa96f94ad25c73ad4f022087cc14a76da
tamtran <tam.tran@asnet.com.vn>
2017-11-14 17:51:19 +0700

    Handle streamer sidebar

--- udemy-practice/src/app/home/home.module.ts
+++ udemy-practice/src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { HomeRouting } from './home-routing.module';
 import { AuthModule } from '../auth/auth.module';
+import { StreamerSidebarComponent } from './streamer-sidebar/streamer-sidebar.component';
 
 export const COMMON_MODULE: any[] = [
   AuthModule
 ];
 
 export const HOME_COMPONENT: any[] = [
-  HomeComponent
+  HomeComponent,
+  StreamerSidebarComponent
 ];
 
 @NgModule({
@@ -16,7 +18,7 @@ export const HOME_COMPONENT: any[] = [
     ...COMMON_MODULE
   ],
   declarations: [
-    ...HOME_COMPONENT
+    ...HOME_COMPONENT,
   ]
 })
 export class HomeModule { }
---
+++ udemy-practice/src/app/home/streamer-sidebar/streamer-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'streamer-sidebar',
+  templateUrl: './streamer-sidebar.component.html',
+  styleUrls: ['./streamer-sidebar.component.scss']
+})
+export class StreamerSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
