0ec8409ea0c28cc73f769a3028a9702a0a65b02c
tamtran <tam.tran@asnet.com.vn>
2017-11-20 18:50:27 +0700

    Update footer

--- udemy-practice/src/app/shared/main-footer/main-footer.component.ts
+++ udemy-practice/src/app/shared/main-footer/main-footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-footer',
+  selector: 'main-footer',
   templateUrl: './main-footer.component.html',
   styleUrls: ['./main-footer.component.scss']
 })
