86968344cc2e2d976d1683158227d94c9529526a
tamtran <tam.tran@asnet.com.vn>
2017-12-11 18:06:05 +0700

    Add structure example decorators

---
+++ example-decorators/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component,
+  ChangeDetectionStrategy
+} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+  title = 'app';
+}
