5f50ac18a60d657039236fca344b1c7df9c61096
tamtran <tam.tran@asnet.com.vn>
2017-12-12 18:42:46 +0700

    Handle simple decorators show logger on component

---
+++ example-decorators/src/app/core/decorators/log.decorator.ts
@@ -0,0 +1,12 @@
+/**
+ * Handle simple decorators show log
+ * @param prefix the title about component
+ */
+export function LogDecorator(prefix?: string) {
+  console.log(prefix);
+  return function (target) {
+    console.log('Our decorated class', target);
+  };
+}
+
+
