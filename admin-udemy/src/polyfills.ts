c4cbcb2abe215643b793645111711d2e67e0938a
tamtran <tam.tran@asnet.com.vn>
2017-12-18 18:29:14 +0700

    Add json service

--- admin-udemy/src/polyfills.ts
+++ admin-udemy/src/polyfills.ts
@@ -50,6 +50,9 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+
+import 'reflect-metadata';
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
