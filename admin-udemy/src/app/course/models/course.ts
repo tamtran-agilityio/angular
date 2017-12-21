76130d43fdef5897938e956f7036834f764a34a6
tamtran <tam.tran@asnet.com.vn>
2017-12-21 18:41:27 +0700

    Add model course

---
+++ admin-udemy/src/app/course/models/course.ts
@@ -0,0 +1,21 @@
+export interface Course {
+  id: number;
+  type: string;
+  categoryId: number;
+  topicId: number;
+  name: string;
+  title?: string;
+  subtitle?: string;
+  images: string;
+  learning: string;
+  description: string;
+  requirements: string;
+  teacherId: number;
+  teacher: any;
+  rating: number;
+  price: number;
+  discountPrice: number;
+  date: string;
+  language?: string;
+
+}
