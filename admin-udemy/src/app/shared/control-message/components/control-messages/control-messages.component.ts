3b881e0a774e3c1199d9b1cf59e0636ad101d617
tamtran <tam.tran@asnet.com.vn>
2017-12-26 16:31:11 +0700

    Fix show error message component

--- admin-udemy/src/app/shared/control-message/components/control-messages/control-messages.component.ts
+++ admin-udemy/src/app/shared/control-message/components/control-messages/control-messages.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit,
   Input,
+  ChangeDetectorRef,
   ChangeDetectionStrategy
 } from '@angular/core';
 import {
@@ -16,25 +17,24 @@ import {
   selector: 'control-messages',
   templateUrl: './control-messages.component.html',
   styleUrls: ['./control-messages.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ControlMessagesComponent implements OnInit {
-  errorMessage: string;
   @Input() control: FormControl;
   constructor(
-    private appConfig: AppConfigService
+    private appConfig: AppConfigService,
+    private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
   }
 
-  get errorMessages() {
+  get errorMessage() {
     for (const propertyName in this.control.errors) {
       if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
         return this.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
       }
     }
-
     return null;
   }
 
